41e3e0b63b32631d4f5938620f54f4060fbf8e4a
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-04-18 11:53:03 +0600

    trigger a task

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from tasks.sample_tasks import create_task
+
 
 def home(request):
     return render(request, "home.html")
@@ -11,7 +13,8 @@ def home(request):
 def run_task(request):
     if request.POST:
         task_type = request.POST.get("type")
-        return JsonResponse({"task_type": task_type}, status=202)
+        task = create_task.delay(int(task_type))
+        return JsonResponse({"task_id": task.id}, status=202)
 
 
 @csrf_exempt
